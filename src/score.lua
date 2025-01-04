88fa2b539fffcc7c57f7092f3e323920b0651a76
Hugo Stackhouse <hugo.stackhouse@sydney.edu.au>
2025-01-04 22:19:47 +1100

    adjusted score function to keep crops above max growth

--- src/score.lua
+++ src/score.lua
@@ -4,15 +4,18 @@ local config = require("config")
 
 local function evalCrop(scan)
     local res_score
+    local growth_score
     local name = scan.crop.name
 
     res_score = -math.abs(scan.crop.resistance - config.resistance_target)
 
     if scan.crop.growth > config.max_growth then
-        return db.WORST
+        growth_score = 2 * config.max_growth - scan.crop.growth
+    else
+        growth_score = scan.crop.growth
     end
     
-    local score =  math.max(0, scan.crop.growth + scan.crop.gain + res_score) -- -1 is empty, so literally any correct crop must be better than that
+    local score =  math.max(0, growth_score + scan.crop.gain + res_score) -- -1 is empty, so literally any correct crop must be better than that
 
     return name, score
 
