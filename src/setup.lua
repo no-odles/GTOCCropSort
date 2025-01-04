6e38762b7c54a0c21f550c3bdfaffd093ff89a18
Hugo Stackhouse <hugo.stackhouse@sydney.edu.au>
2025-01-04 23:00:34 +1100

    fixed install script

--- src/setup.lua
+++ src/setup.lua
@@ -6,8 +6,10 @@ local scripts = {
     'config.lua',
     'database.lua',
     'inventory.lua',
+    'inventory_lowmem.lua',
     'score.lua',
     'sort.lua',
+    'sort_lm.lua',
     'uninstall.lua'
 }
 
